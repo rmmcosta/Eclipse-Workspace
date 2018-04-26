b89a14b6457220cd2d84227a3ddd02e6bf4148a1
rmmcosta <ricardocosta101085@gmail.com>
2018-04-26 11:23:12 +0100

    first commit

---
+++ Test/src/Program.java
@@ -0,0 +1,11 @@
+import onemore.*;
+
+public class Program {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Hello");
+		System.out.println(onemoretest.printThings());
+	}
+
+}
---
+++ Test/src/onemore/onemoretest.java
@@ -0,0 +1,7 @@
+package onemore;
+
+public class onemoretest {
+	public static String printThings() {
+		return "Hello from the one more test";
+	}
+}
